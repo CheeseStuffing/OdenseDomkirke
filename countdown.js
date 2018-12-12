407f83a3ffaa00ad1f1dc6e1efb0f28b3ea38e1a
CheeseStuffing <42998889+CheeseStuffing@users.noreply.github.com>
2018-12-12 11:48:37 +0100

    file cleanup

---
+++ countdown.js
@@ -0,0 +1,29 @@
+// Ind sæt dato, for den dag vi tæller ned til 
+    var countDownDate = new Date("Apr 23, 2019 10:00:00").getTime();
+// opdateringer tæller ned hvert sekund
+    var x = setInterval(function() {
+// Dagens dato og klokkeslæt 
+    var now = new Date().getTime();
+                
+// Finder afstanden mellem nu og datoen vi tæller ned til 
+    var distance = countDownDate - now;
+                
+// Tidsberengninger for dage timer, minutter og sekunder.
+    var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+    var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+                
+// udføre resultatet i det element med id'et "countdown"
+    document.getElementById("count").innerHTML = days + "d " + hours + "t " + minutes + "m " ;
+    // + seconds + "s " er fjernet
+                
+// Når nedtællingen er over, vil det stå denne tekst 
+    if (distance < 0 && distance < -25200) {
+            clearInterval(x);
+            document.getElementById("count").innerHTML = "EXPIRED";
+        }
+        /*else if (distance <= -25200) {
+                document.getElementById("count").innerHTML = "EXPIRED2";
+        }*/
+    }, );
---
+++ smoothscroll.js
@@ -0,0 +1,22 @@
+window.smoothScroll6 = function(target) {
+    var scrollContainer = target;
+    do { //find scroll container
+        scrollContainer = scrollContainer.parentNode;
+        if (!scrollContainer) return;
+        scrollContainer.scrollTop += 1;
+    } while (scrollContainer.scrollTop == 0);
+
+    var targetY = 0;
+    do { //find toppen af ID relativ til scroll container
+        if (target == scrollContainer) break;
+        targetY += target.offsetTop;
+    } while (target = target.offsetParent);
+
+    scroll = function(c, a, b, i) {
+        i++; if (i > 30) return;
+        c.scrollTop = a + (b - a) / 30 * i;
+        setTimeout(function(){ scroll(c, a, b, i); }, 10);
+    }
+    // start scroll
+    scroll(scrollContainer, scrollContainer.scrollTop, targetY, 0);
+}
